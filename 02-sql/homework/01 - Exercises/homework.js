2cf781acc1a63932cae8d9c63841c109b6e4ac47
claudiop1975 <claudio.parache@gmail.com>
2023-03-05 20:41:50 -0300

    M4 dbms y sql listo

--- 02-sql/homework/01 - Exercises/homework.js
+++ 02-sql/homework/01 - Exercises/homework.js
@@ -1,16 +1,16 @@
 /* ¡Escribe tus comandos en este archivo! */
 
-const ejercicio02 = '';
+const ejercicio02 = 'SELECT * FROM movies WHERE duration < 90;';
 
-const ejercicio03 = '';
+const ejercicio03 = 'SELECT * FROM movies WHERE year > 1929 AND year < 1941;';
 
-const ejercicio04 = '';
+const ejercicio04 = 'SELECT * FROM movies WHERE title LIKE '%til%';';
 
-const ejercicio05 = '';
+const ejercicio05 = 'SELECT * FROM movies WHERE cardinality(actors)=1;';
 
-const ejercicio06 = '';
+const ejercicio06 = 'SELECT title, AVG(rating) FROM movies, unnest(ratings) rating GROUP BY title;';
 
-const ejercicio07 = '';
+const ejercicio07 = 'SELECT actors FROM movies WHERE title LIKE "%Fast and%" AND year=2016;';
 
 module.exports = {
    ejercicio02,
